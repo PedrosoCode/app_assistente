fad85fe36d2f12089ddeb8873f0bc63bdf1a2a41
Gabriel <Gabrielpedrofc@gmail.com>
2024-04-25 07:27:24 -0300

    add bootstrap e navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import './bootstrap/bootstrap/css/bootstrap.min.css'
+import './bootstrap/bootstrap/js/bootstrap.min'
 
 function App() {
   return (
