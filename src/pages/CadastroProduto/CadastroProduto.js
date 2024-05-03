9a5dede2ac48a6e5bcae0569b65ec49c2e1231b1
Gabriel <Gabrielpedrofc@gmail.com>
2024-05-02 21:44:51 -0300

    Rascunho Cadastro de Produto

--- src/pages/CadastroProduto/CadastroProduto.js
+++ src/pages/CadastroProduto/CadastroProduto.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom'; // Importando useNavigate
 import { ProtectedRoute } from '../../components/ProtectedRoute';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function CadastroProduto() {
     const navigate = useNavigate(); // Hook para navegação
@@ -23,6 +24,36 @@ function CadastroProduto() {
     }, []);
 
 
+        const [file, setFile] = useState(null);
+        const handleFileChange = event => {
+          setFile(event.target.files[0]);
+        };
+      
+        const handleFormSubmit = async event => {
+          event.preventDefault();
+      
+          if (!file) {
+            alert('Por favor, selecione uma imagem para fazer upload.');
+            return;
+          }
+      
+          const formData = new FormData();
+          formData.append('image', file);
+      
+          try {
+            const response = await axios.post('http://localhost:3042/upload', formData, {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+              },
+            });
+            alert('Upload realizado com sucesso: ' + response.data.path);
+          } catch (error) {
+            console.error('Erro no upload:', error);
+            alert('Erro ao fazer upload da imagem.');
+          }
+        };
+
+
 
     return (
         <ProtectedRoute>
@@ -35,6 +66,28 @@ function CadastroProduto() {
                     </option>
                 ))}
             </select>
+
+            <div class="mb-3">
+                <label for="NomeProduto" class="form-label">Email address</label>
+                <input type="text" class="form-control" id="NomeProduto" placeholder="Nome do Produto"></input>
+            </div>
+            <div class="mb-3">
+                <label for="TextoProduto" class="form-label">Insira uma descrição sobre o produto</label>
+                <textarea class="form-control" id="TextoProduto" rows="3"></textarea>
+            </div>
+
+            <div>
+      <form onSubmit={handleFormSubmit}>
+        <h1>Selecione a imagem para ser a capa do produto</h1>
+        <input type="file" onChange={handleFileChange} accept="image/*" />
+        <button type="submit">Upload</button>
+      </form>
+      <form onSubmit={handleFormSubmit}>
+        <h1>Selecione uma ou mais imagems para serem detalhes do produto</h1>
+        <input type="file" onChange={handleFileChange} accept="image/*" />
+        <button type="submit">Upload</button>
+      </form>
+    </div>
 
 
         </ProtectedRoute>
